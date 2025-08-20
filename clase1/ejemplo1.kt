e0a586b3201deffc89871271b3e88cbefecd147b
miguelcamposdasilva <migu.campos@duocuc.cl>
2025-08-20 12:51:49 -0400

    se agrega clase1

--- clase1/ejemplo1.kt
+++ clase1/ejemplo1.kt
@@ -1,5 +1,16 @@
 package clase1
 
 fun main(){
-    println("hola mundo")
+    println("Hola mundooo!!")
+    val A: Int = 1
+    var B: Int = 2
+    B++
+    var C: Int = A+B
+    println("La respuesta es :" + C)
+
+    var c: String="clase"
+    println(c.length)
+
 }
+
+
