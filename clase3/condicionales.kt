9fbd46d4c78d5889ca13421211d913d422528085
unknown <Cetecom@SJAPCA03LC0713.duoc.acad>
2025-08-27 11:18:35 -0400

    Clase 3

---
+++ clase3/condicionales.kt
@@ -0,0 +1,22 @@
+package clase3
+
+fun main(){
+
+    validacionEdad(21)
+
+    validacionEdad(14)
+
+    validacionEdad(11)
+
+    validacionEdad(60)
+}
+
+fun validacionEdad(edad: Int){
+    println("La edad es de $edad")
+    if(edad >= 18){
+        println("Es mayor de edad")
+    }
+    else{
+        println("Es menor de edad")
+    }
+}
---
+++ out/production/DAM/README.md
@@ -0,0 +1,2 @@
+# Este repositorio contiene todas las clases de Desarrollo de aplicaciones moviles 
+# --miguelcamposdasilva
